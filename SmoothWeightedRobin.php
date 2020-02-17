bb3548da5d6850836a0a6489b7154fcb7eddf670
guxiangming <403502556@qq.com>
2020-02-18 00:10:47 +0800

    修改日志与平滑加权轮询算法优化

--- SmoothWeightedRobin.php
+++ SmoothWeightedRobin.php
@@ -1,4 +1,44 @@
+<?php 
+
+require "./RobinInterface.php";
+
 class SmoothWeightedRebin implements RobinInterface
 {
-    
+    private $services=[];
+
+    private $total;
+
+    private $currentPos=-1;
+
+    public function init(array $services){
+        foreach($services as $ip=>$weight){
+            $this->services[]=[
+                'ip'=>$ip,
+                'weight'=>$weight,
+                'current_weight'=>$weight
+            ];
+        }
+
+        $this->total=count($this->services);
+    }
+
+    public function next(){
+
+    }
+
+    public function getMaxCurrentWeightPos(){
+
+    }
+
+    public function getSumWeight(){
+
+    }
+
+    public function getCurrentWeight(){
+
+    }
+
+    public function recoverCurrentWeight(){
+        
+    }
 }
