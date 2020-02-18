4717f61236e9fc7f39431ded90e146b36988a648
guxiangming <403502556@qq.com>
2020-02-19 00:23:39 +0800

    完善平滑轮询算法

--- SmoothWeightedRobin.php
+++ SmoothWeightedRobin.php
@@ -1,6 +1,6 @@
 <?php 
 
-require "./RobinInterface.php";
+require "RobinInterface.php";
 
 class SmoothWeightedRebin implements RobinInterface
 {
@@ -23,22 +23,43 @@ class SmoothWeightedRebin implements RobinInterface
     }
 
     public function next(){
-
+        $this->currentPos=$this->getMaxCurrentWeightPos();
+        $currentWeight = intval($this->getCurrentWeight($this->currentPos)) - intval($this->getSumWeight());
+        $this->setCurrentWeight($this->currentPos, $currentWeight);
+        $this->recoverCurrentWeight();
+        return $this->services[$this->currentPos]['ip'];
     }
 
     public function getMaxCurrentWeightPos(){
-
+        $currentWeight=$pos=0;
+        foreach($this->services as $index=>$service){
+            if($service['current_weight'] > $currentWeight){
+                $currentWeight=$service['current_weight'];
+                $pos=$index;
+            }
+        }
+        return $pos;
     }
 
     public function getSumWeight(){
-
+        $sum=0;
+        foreach($this->services as $service){
+            $sum+=intval($service['weight']);
+        }
+        return $sum;
     }
 
-    public function getCurrentWeight(){
+    public function setCurrentWeight($pos,$weight){
+        $this->services[$pos]['current_weight']=$weight;
+    }
 
+    public function getCurrentWeight($pos){
+        return $this->services[$pos]['current_weight'];
     }
 
     public function recoverCurrentWeight(){
-        
+        foreach ($this->services as $index => &$service) {
+            $service['current_weight'] += intval($service['weight']);
+        }
     }
 }
